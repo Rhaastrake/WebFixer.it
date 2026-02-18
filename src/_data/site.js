e03a5cb37ae4a9a6055665dc5e45a9f9c290c720
Rhaastrake <garofalomichele2001@gmail.com>
2026-02-18 20:10:40 +0100

    Fixed dinamic title, desc and keywords

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,18 +1,37 @@
-const { title } = require("../../../roberta/website_dottoressa_tene/src/_data/site");
-
 module.exports = {
+  // site_name: "Webfixer",
+  // title: "Webfixer.it | Assistenza tenica per PC e sistemi informatici",
+  // description:
+  //   "Sito vetrina di Michele Garofalo, specializzato in software e assistenza hardware di vario genere",
+  // domain: "webfixer.it",
+  // url: "https://webfixer.it", // It WILL NOT end with a slash /
+  // lang: "it",
+  // author: "Michele Garofalo",
+  // logo: "/assets/img/logo",
+  // image_width: [320, 480, 720, 1280, 1920, 2048, 2560, 3840, 4096, 7680],
+  // copyright: {
+  //   year: "2026",
+  //   text: "Michele Garofalo - Tutti i diritti riservati",
+  //   privacy: "",//link alla privacy
+  //   cookie: "",//link ai cookie
+  //   cookieControls:
+  //     "",//link ai cookie controls
+  //   terms: "",//Link ai termini
+  // },
   homepage: {
     seo: {
       title: "Webfixer - Homepage",
       description: "Assistenza tecnica per PC e sistemi informatici.",
-      keywords: "Riparazione computer Bari, computer rotto bari, computer lento, pulizia computer bari, negozio computer bari, computer si spegne da solo, michele garofalo, tecnico pc bari",
+      keywords:
+        "Riparazione computer Bari, computer rotto bari, computer lento, pulizia computer bari, negozio computer bari, computer si spegne da solo, michele garofalo, tecnico pc bari",
     },
   },
   about: {
     seo: {
       title: "Webfixer - Curriculum",
       description: "Assistenza tecnica per PC e sistemi informatici.",
-      keywords: "michele garofalo, programmatore, tenico pc, curriculum, chi sono",
+      keywords:
+        "michele garofalo, programmatore, tenico pc, curriculum, chi sono, ICT, specialista, hardware, software, tecnico",
     },
   },
   news: {
@@ -24,7 +43,7 @@ module.exports = {
   },
   404: {
     seo: {
-        title: "Webfixer - 404"
-    }
-  }
+      title: "Webfixer - 404",
+    },
+  },
 };
